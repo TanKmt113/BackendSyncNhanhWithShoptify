0ce93312ffc7b186a8b44502a8d8e73c6b3294a1
tankmt113 <dotrongtan113@gmail.com>
2026-04-10 14:53:08 +0700

    Update

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ app.use("/api", routes);
 app.get("/api/health", (req, res) => {
   res.json({
     status: "ok",
-    message: "API is running 🚀",
+    message: "API is running 🚀 113",
     time: new Date()
   });
 });
